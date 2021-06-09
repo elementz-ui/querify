8516c482486ea344dcf978e022c68bfaf96110ac
elis-k <me@elis.cc>
2021-06-10 00:17:58 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='elementz_querify',
-    version='1.0.0',
+    version='1.0.1',
     description='SQL Query Builder for Elementz Table',
     long_description='',
     license='GPL 3.0',
